7e7bbca2159c5ab0ecc7306537926956e3956615
prateeksarangi <sarangiprateek80@gmail.com>
2020-03-14 23:03:54 +0530

    Data reading successful

--- NNHeart.R
+++ NNHeart.R
@@ -1,15 +1,23 @@
+library(dplyr)
+
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
 
-heart <- read.csv("~/Downloads/HeartDisease/heart.csv")
 
-max(heart["chol"])
-min(heart["chol"])
+heart <- read.csv("~/HeartDisease/heart.csv")
+
+# max(heart["chol"])
+# min(heart["chol"])
 
 dfNorm <- as.data.frame(lapply(heart["age"], normalize))
 heart["age"] <- dfNorm
+heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, between(x, 0.0, 0.25), 0.1)}))
+heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, between(x, 0.25, 0.6), 0.4)}))
+heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, between(x, 0.5, 0.75), 0.6)}))
+heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, between(x, 0.75, 1), 0.9)}))
+
 
 dfNorm <- as.data.frame(lapply(heart["trestbps"], normalize))
 heart["trestbps"] <- dfNorm
@@ -17,6 +25,12 @@ heart["trestbps"] <- dfNorm
 #To be changed
 dfNorm <- as.data.frame(lapply(heart["chol"], normalize))
 heart["chol"] <- dfNorm
+heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.0, 0.2), 0.1)}))
+heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.2, 0.4), 0.3)}))
+heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.4, 0.6), 0.5)}))
+heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.6, 0.8), 0.7)}))
+heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.8, 1), 0.9)}))
+
 
 dfNorm <- as.data.frame(lapply(heart["thalach"], normalize))
 heart["thalach"] <- dfNorm
@@ -24,6 +38,11 @@ heart["thalach"] <- dfNorm
 #To be changed
 dfNorm <- as.data.frame(lapply(heart["cp"], normalize))
 heart["cp"] <- dfNorm
+heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(x, 0.0, 0.25), 0.1)}))
+heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(x, 0.25, 0.5), 0.4)}))
+heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(x, 0.5, 0.75), 0.6)}))
+heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(x, 0.75, 1), 0.9)}))
+
 
 #To be changed
 dfNorm <- as.data.frame(lapply(heart["thal"], normalize))
@@ -44,20 +63,17 @@ heart["restecg"] <- as.data.frame(lapply(heart["restecg"], function(x){replace(x
 heart["exang"] <- as.data.frame(lapply(heart["exang"], function(x){replace(x, x == 0, 0.1)}))
 heart["exang"] <- as.data.frame(lapply(heart["exang"], function(x){replace(x, x == 1, 0.9)}))
 
-smp_size <- floor(0.75 * nrow(heart))
-train_ind_rand <- sample(seq_len(nrow(heart)), size = smp_size)
-
-trainrand <- heart[train_ind_rand, ]
-testrand <- heart[-train_ind_rand, ]
-
-trainseq <- heart[1:227, ]
-testseq <- heart[227:303, ]
-
-write.csv(heart, "~/Downloads/HeartDisease/heart1.csv", row.names = FALSE)
-write.csv(trainrand, "~/Downloads/HeartDisease/trainrand.csv", row.names = FALSE)
-write.csv(testrand, "~/Downloads/HeartDisease/testrand.csv", row.names = FALSE)
-write.csv(trainseq, "~/Downloads/HeartDisease/trainseq.csv", row.names = FALSE)
-write.csv(testseq, "~/Downloads/HeartDisease/testseq.csv", row.names = FALSE)
-
-max(heart["chol"])
-min(heart["chol"])
+# smp_size <- floor(0.75 * nrow(heart))
+# train_ind_rand <- sample(seq_len(nrow(heart)), size = smp_size)
+# 
+# trainrand <- heart[train_ind_rand, ]
+# testrand <- heart[-train_ind_rand, ]
+# 
+# trainseq <- heart[1:227, ]
+# testseq <- heart[227:303, ]
+# 
+# write.csv(heart, "~/HeartDisease/heart1.csv", row.names = FALSE)
+# write.csv(trainrand, "~/HeartDisease/trainrand.csv", row.names = FALSE)
+# write.csv(testrand, "~/HeartDisease/testrand.csv", row.names = FALSE)
+# write.csv(trainseq, "~/HeartDisease/trainseq.csv", row.names = FALSE)
+# write.csv(testseq, "~/HeartDisease/testseq.csv", row.names = FALSE)
