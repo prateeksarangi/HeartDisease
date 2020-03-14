218636194f384daf3f3ac5bfa5845b221c3eafcb
prateeksarangi <sarangiprateek80@gmail.com>
2020-03-14 23:16:21 +0530

    Data Normalized

--- NNHeart.R
+++ NNHeart.R
@@ -1,15 +1,12 @@
 library(dplyr)
 
+
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
 
-
 heart <- read.csv("~/HeartDisease/heart.csv")
-
-# max(heart["chol"])
-# min(heart["chol"])
 
 dfNorm <- as.data.frame(lapply(heart["age"], normalize))
 heart["age"] <- dfNorm
@@ -18,11 +15,12 @@ heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, betwee
 heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, between(x, 0.5, 0.75), 0.6)}))
 heart["age"] <- as.data.frame(lapply(heart["age"], function(x){replace(x, between(x, 0.75, 1), 0.9)}))
 
-
 dfNorm <- as.data.frame(lapply(heart["trestbps"], normalize))
 heart["trestbps"] <- dfNorm
+heart["trestbps"] <- as.data.frame(lapply(heart["trestbps"], function(x){replace(x, between(x, 0.0, 0.33), 0.2)}))
+heart["trestbps"] <- as.data.frame(lapply(heart["trestbps"], function(x){replace(x, between(x, 0.33, 0.67), 0.6)}))
+heart["trestbps"] <- as.data.frame(lapply(heart["trestbps"], function(x){replace(x, between(x, 0.67, 1), 1)}))
 
-#To be changed
 dfNorm <- as.data.frame(lapply(heart["chol"], normalize))
 heart["chol"] <- dfNorm
 heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.0, 0.2), 0.1)}))
@@ -31,11 +29,9 @@ heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, betw
 heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.6, 0.8), 0.7)}))
 heart["chol"] <- as.data.frame(lapply(heart["chol"], function(x){replace(x, between(x, 0.8, 1), 0.9)}))
 
-
 dfNorm <- as.data.frame(lapply(heart["thalach"], normalize))
 heart["thalach"] <- dfNorm
 
-#To be changed
 dfNorm <- as.data.frame(lapply(heart["cp"], normalize))
 heart["cp"] <- dfNorm
 heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(x, 0.0, 0.25), 0.1)}))
@@ -44,12 +40,17 @@ heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(
 heart["cp"] <- as.data.frame(lapply(heart["cp"], function(x){replace(x, between(x, 0.75, 1), 0.9)}))
 
 
-#To be changed
 dfNorm <- as.data.frame(lapply(heart["thal"], normalize))
 heart["thal"] <- dfNorm
+heart["thal"] <- as.data.frame(lapply(heart["thal"], function(x){replace(x, between(x, 0.0, 0.33), 0.2)}))
+heart["thal"] <- as.data.frame(lapply(heart["thal"], function(x){replace(x, between(x, 0.33, 0.67), 0.6)}))
+heart["thal"] <- as.data.frame(lapply(heart["thal"], function(x){replace(x, between(x, 0.67, 1), 0.9)}))
 
 dfNorm <- as.data.frame(lapply(heart["slope"], normalize))
 heart["slope"] <- dfNorm
+
+dfNorm <- as.data.frame(lapply(heart["ca"], normalize))
+heart["ca"] <- dfNorm
 
 heart["sex"] <- as.data.frame(lapply(heart["sex"], function(x){replace(x, x == 0, 0.1)}))
 heart["sex"] <- as.data.frame(lapply(heart["sex"], function(x){replace(x, x == 1, 0.9)}))
