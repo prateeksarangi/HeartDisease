0718af763462f35fa58f60d177630e98ba490af7
prateeksarangi <sarangiprateek80@gmail.com>
2020-03-15 10:14:28 +0530

    Loss and Accuracy plots added

--- NNHeart.py
+++ NNHeart.py
@@ -3,6 +3,7 @@ from keras.layers import Dense
 from pandas import read_csv
 import matplotlib.pyplot as plt
 
+
 trainPath = '~/HeartDisease/trainrand.csv'
 trainData = read_csv(trainPath)
 
@@ -17,16 +18,20 @@ Y_train = arrayTrain[:,13]
 
 X_test = arrayTest[:, 0:13]
 Y_test = arrayTest[:, 13]
+
 model = Sequential()
-model.add(Dense(8, input_dim=13, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='sigmoid', use_bias=True))
-model.add(Dense(8, init='uniform', activation='sigmoid', use_bias=True))
+model.add(Dense(12, input_dim=13, init='uniform', activation='relu'))
+model.add(Dense(12, init='uniform', activation='sigmoid', use_bias=True))
+model.add(Dense(10, init='uniform', activation='sigmoid', use_bias=True))
 model.add(Dense(8, init='uniform', activation='sigmoid', use_bias=True))
 model.add(Dense(8, init='uniform', activation='sigmoid', use_bias=True))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 history = model.fit(X_train, Y_train, nb_epoch = 800)
+
+scores = model.evaluate(X_test, Y_test)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 plt.plot(history.history['accuracy'])
 plt.title('Model accuracy')
@@ -36,12 +41,8 @@ plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
 plt.plot(history.history['loss'])
-#plt.plot(history.history['val_loss'])
 plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-
-scores = model.evaluate(X_test, Y_test)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
