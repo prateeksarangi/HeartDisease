9c64ff3e2e866788a6f018b1ed265ac6a2f79512
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-08-17 17:59:01 +0530

    Performance matrix added

--- NNHeart.py
+++ NNHeart.py
@@ -34,25 +34,47 @@ scores = model.evaluate(X_test, Y_test)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 
-MyList = history.history['accuracy']
-MyFile=open('Accuracy.txt','a')
+# MyList = history.history['accuracy']
+# MyFile=open('Accuracy.txt','a')
+#
+# for element in MyList:
+#     MyFile.write(str(element))
+#     MyFile.write('\n')
+# MyFile.close()
+#
+#
+# MyList = history.history['loss']
+# MyFile=open('MSE.txt','a')
+#
+# for element in MyList:
+#     MyFile.write(str(element))
+#     MyFile.write('\n')
+# MyFile.close()
 
-for element in MyList:
+from sklearn.metrics import confusion_matrix
+pred = model.predict_classes(X_test)
+
+CM = confusion_matrix(Y_test, pred)
+from mlxtend.plotting import plot_confusion_matrix
+fig, ax = plot_confusion_matrix(conf_mat=CM,  figsize=(5, 5))
+plt.show()
+
+FN = CM[0][0]
+FP = CM[1][0]
+TN = CM[0][1]
+TP = CM[1][1]
+
+sensitivity = TP/(TP+FN)
+specificity = TN/(TN+FP)
+
+MyFile=open('performance_Matrix.txt','a')
+
+for element in CM:
     MyFile.write(str(element))
     MyFile.write('\n')
 MyFile.close()
 
-
-MyList = history.history['loss']
-MyFile=open('MSE.txt','a')
-
-for element in MyList:
-    MyFile.write(str(element))
-    MyFile.write('\n')
-MyFile.close()
-
-
-
+print(CM)
 plt.plot(history.history['accuracy'])
 plt.title('Model accuracy')
 plt.ylabel('Accuracy')
